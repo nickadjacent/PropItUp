852ed6028ce72863394b2500135ec16bda1251be
Nick_Ram <60027609+nickadjacent@users.noreply.github.com>
2020-03-03 18:42:07 -0800

    Updated

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
+    constructor({ first_name, last_name, age, hair_color }) {
+        super();
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.age = age
+        this.hair_color = hair_color;
+        this.state = {
+            clickCount: this.age
+        }
+
+    }
+
+    handleClick = () => {
+        this.setState({
+            clickCount: this.state.clickCount + 1
+        })
+    }
+
+
     render = () => {
         return (
             <div>
                 <h1>{this.props.last_name}, {this.props.first_name}</h1>
-                <p>Age: {this.props.age}</p>
+                <p>Age: {this.state.clickCount}</p>
                 <p>Hair Color: {this.props.hair_color}</p>
+                <button onClick={this.handleClick}>Birthday Button for {this.first_name} {this.last_name}</button>
             </div >
         );
 
